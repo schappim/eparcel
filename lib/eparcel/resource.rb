a660f3dedd552619ca0d73d73159b1c41e189024
Marcus Schappi <marcus@schappi.com>
2022-08-26 10:00:43 +1000

    code for auspost validation

--- lib/eparcel/resource.rb
+++ lib/eparcel/resource.rb
@@ -10,22 +10,92 @@ module Eparcel
     private
 
     def get_request(url, params: {}, headers: {})
+      puts "\n\n"
+      puts "\n\n"
+      puts "==============================================="
+      puts "METHOD: GET"
+      puts "-----------------------------------------------"
+      puts "url: #{url}"
+      puts "-----------------------------------------------"
+      puts "headers: #{headers.to_json}"
+      puts "-----------------------------------------------"
+      puts "==============================================="
+      puts "\n\n"
+      puts "\n\n"
+
       handle_response client.connection.get(url, params, headers)
     end
 
     def post_request(url, body:, headers: {})
+
+      puts "\n\n"
+      puts "\n\n"
+      puts "==============================================="
+      puts "METHOD: POST"
+      puts "-----------------------------------------------"
+      puts "url: #{url}"
+      puts "-----------------------------------------------"
+      puts "headers: #{headers.to_json}"
+      puts "-----------------------------------------------"
+      puts "body:\n\n"
+      puts body.to_json
+      puts "==============================================="
+      puts "\n\n"
+      puts "\n\n"
+
       handle_response client.connection.post(url, body, headers)
     end
 
     def patch_request(url, body:, headers: {})
+      puts "\n\n"
+      puts "\n\n"
+      puts "==============================================="
+      puts "METHOD: PATCH"
+      puts "-----------------------------------------------"
+      puts "url: #{url}"
+      puts "-----------------------------------------------"
+      puts "headers: #{headers.to_json}"
+      puts "-----------------------------------------------"
+      puts "body:\n\n"
+      puts body.to_json
+      puts "==============================================="
+      puts "\n\n"
+      puts "\n\n"
       handle_response client.connection.patch(url, body, headers)
     end
 
     def put_request(url, body:, headers: {})
+      puts "\n\n"
+      puts "\n\n"
+      puts "==============================================="
+      puts "METHOD: PUT"
+      puts "-----------------------------------------------"
+      puts "url: #{url}"
+      puts "-----------------------------------------------"
+      puts "headers: #{headers.to_json}"
+      puts "-----------------------------------------------"
+      puts "body:\n\n"
+      puts body.to_json
+      puts "==============================================="
+      puts "\n\n"
+      puts "\n\n"
       handle_response client.connection.put(url, body, headers)
     end
 
     def delete_request(url, params: {}, headers: {})
+      puts "\n\n"
+      puts "\n\n"
+      puts "==============================================="
+      puts "METHOD: DELETE"
+      puts "-----------------------------------------------"
+      puts "url: #{url}"
+      puts "-----------------------------------------------"
+      puts "headers: #{headers.to_json}"
+      puts "-----------------------------------------------"
+      puts "==============================================="
+      puts "\n\n"
+      puts "\n\n"
+
       handle_response client.connection.delete(url, params, headers)
     end
 
